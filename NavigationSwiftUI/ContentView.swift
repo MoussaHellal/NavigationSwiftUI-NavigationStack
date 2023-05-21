b0d6d868d2266084d93967312456b78e3b0dfb8c
Moussa Hellal <moussa@sane.agency>
2023-05-21 14:25:08 +0100

    Initial Commit

---
+++ NavigationSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationSwiftUI
+//
+//  Created by Moussa on 21/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
